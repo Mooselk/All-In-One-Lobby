dff62e4def39b3c6dccac8d6ab791c757da23ae8
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-03 11:56:51 -0700

    MineStat API

--- src/me/kate/lobby/items/compass/ping/PingServersBackground.java
+++ src/me/kate/lobby/items/compass/ping/PingServersBackground.java
@@ -8,9 +8,7 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 
 import me.kate.lobby.Main;
-import me.kate.lobby.items.compass.Compass;
-import me.kate.lobby.utils.ServerPinger;
-import me.kate.lobby.utils.ServerPinger.Server;
+import me.kate.lobby.utils.MineStat;
 
 public class PingServersBackground extends Thread {
 
@@ -21,49 +19,40 @@ public class PingServersBackground extends Thread {
 		while (true) {
 			try {
 				for (final String key : config.getConfigurationSection("compass").getKeys(false)) {
-					// Don't overload the CPU or the API
-					try {
-						Thread.sleep(2000);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
+					final ConfigurationSection section1 = config.getConfigurationSection("compass." + key);
+					if (!section1.getBoolean("decoration") && !key.equals("options")) {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						}
+						final ConfigurationSection section = config.getConfigurationSection("compass." + key);
+						if (!section.getBoolean("server.ping-server", false)) {
+							continue;
+						}
+						String ip = section.getString("server.ip");
+						int port = section.getInt("server.port");
+
+						MineStat ms = new MineStat(ip, port, 1);
+
+						String serverName = section.getString("server.server-id");
+
+						Map<String, Object> serverInfo = new HashMap<>();
+
+						serverInfo.put("isOnline", ms.isServerUp());
+
+						if (ms.isServerUp()) {
+							serverInfo.put("online", ms.getCurrentPlayers());
+							serverInfo.put("max", ms.getMaximumPlayers());
+							serverInfo.put("version", ms.getVersion());
+							serverInfo.put("ping", ms.getLatency());
+							Bukkit.getLogger().info("updating: " + key);
+						}
+						Main.SERVER_PLACEHOLDERS.put(serverName, serverInfo);
 					}
-
-					final ConfigurationSection section = config.getConfigurationSection("compass." + key);
-
-					if (!section.getBoolean("server.ping-server", false)) {
-						continue;
-					}
-
-					String ip = section.getString("server.ip");
-					int port = section.getInt("server.port");
-
-					Server server;
-
-					if (Main.getInstance().getConfig().getBoolean("server.external-query", true)) {
-						server = new ServerPinger.ExternalServer(ip, port);
-					} else {
-						int timeout = section.getInt("server.ping-timeout", 100);
-						server = new ServerPinger.InternalServer(ip, port, timeout);
-					}
-
-					String serverName = section.getString("server.server-id");
-
-					Map<String, Object> serverInfo = new HashMap<>();
-					serverInfo.put("isOnline", server.isOnline());
-
-					if (server.isOnline()) {
-						serverInfo.put("online", server.getOnlinePlayers());
-						serverInfo.put("max", server.getMaximumPlayers());
-						
-						serverInfo.put("ping", server.getResponseTimeMillis());
-						Bukkit.getLogger().info("updating");
-					}
-					Main.SERVER_PLACEHOLDERS.put(serverName, serverInfo);
-					Compass.update();
-					//Compass.getPlayer().updateInventory();
-					//Bukkit.getLogger().info("ServerInfo: " + Main.SERVER_PLACEHOLDERS);
 				}
 			} catch (Exception e) {
+				e.printStackTrace();
 				// So the loop doesn't break if an error occurs
 				// Print the error, sleep, try again.
 				try {
