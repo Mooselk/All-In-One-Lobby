9b621ba8a11717c86ccbc908f6920fb9e0b6cf2b
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-06 12:29:19 -0700

    Create configs on startup

--- src/me/kate/lobby/Main.java
+++ src/me/kate/lobby/Main.java
@@ -2,55 +2,73 @@ package me.kate.lobby;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitTask;
 
 import me.kate.lobby.commands.ReloadConfigCommand;
 import me.kate.lobby.commands.SetSpawnCommand;
 import me.kate.lobby.data.Config;
+import me.kate.lobby.data.files.HidePlayersFile;
+import me.kate.lobby.data.files.PlayerSettingsFile;
+import me.kate.lobby.data.files.SelectorFile;
+import me.kate.lobby.data.files.interfaces.IHidePlayerSettings;
+import me.kate.lobby.data.files.interfaces.IPlayerSettings;
+import me.kate.lobby.data.files.interfaces.ISelectorSettings;
 import me.kate.lobby.events.BlockRelatedEvent;
 import me.kate.lobby.events.JoinEvent;
 import me.kate.lobby.events.MobSpawnEvent;
 import me.kate.lobby.events.TouchVoidEvent;
+import me.kate.lobby.items.hideplayers.events.HidePlayersInteractEvent;
 import me.kate.lobby.items.selector.events.SelectorClickEvent;
 import me.kate.lobby.items.selector.events.SelectorGuiEvents;
-import me.kate.lobby.items.selector.ping.PingServersBackground;
 
 public class Main extends JavaPlugin {
+
+	private static Main instance;
 	
-	private static Main plugin;
+	private IPlayerSettings playerSettings = new PlayerSettingsFile();
+	private ISelectorSettings selectorSettings = new SelectorFile();
+	private IHidePlayerSettings hideSettings = new HidePlayersFile();
 	
 	public static final Map<String, Map<String, Object>> SERVER_PLACEHOLDERS = new HashMap<>();
-	
+	public static final Map<UUID, Integer> COOLDOWNS = new HashMap<>();
+	public static final Map<UUID, BukkitTask> TASKS = new HashMap<>();
+
 	public static Main getInstance() {
-		return plugin;
+		return instance;
 	}
-	
+
 	@Override
 	public void onEnable() {
-		plugin = this;
-		new PingServersBackground().start();
+		instance = this;
+		playerSettings.create();
+		selectorSettings.create();
+		hideSettings.create();
+		// new PingServersBackground().start();
 		Config.createConfig();
 		this.registerEvents();
 		this.registerCommands();
 	}
-	
+
 	@Override
 	public void onDisable() {
-		
+
 	}
-	
+
 	private void registerEvents() {
-		getServer().getPluginManager().registerEvents(new JoinEvent(), this);
-		getServer().getPluginManager().registerEvents(new TouchVoidEvent(), this);
-		getServer().getPluginManager().registerEvents(new BlockRelatedEvent(), this);
-		getServer().getPluginManager().registerEvents(new MobSpawnEvent(), this);
-		getServer().getPluginManager().registerEvents(new SelectorGuiEvents(), this);
-		getServer().getPluginManager().registerEvents(new SelectorClickEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new JoinEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new TouchVoidEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new BlockRelatedEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new MobSpawnEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new SelectorGuiEvents(), this);
+		this.getServer().getPluginManager().registerEvents(new SelectorClickEvent(), this);
+		this.getServer().getPluginManager().registerEvents(new HidePlayersInteractEvent(), this);
 	}
-	
+
 	private void registerCommands() {
 		this.getCommand("setspawn").setExecutor(new SetSpawnCommand());
-		this.getCommand("reloadconfig").setExecutor(new ReloadConfigCommand());
+		this.getCommand("reloadselector").setExecutor(new ReloadConfigCommand());
 	}
 }
