bbd690d18a4da35a9a6cb50dbcea90f9bcf31fc6
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-06 12:31:10 -0700

    Hide tool

--- src/me/kate/lobby/items/hideplayers/HidePlayers.java
+++ src/me/kate/lobby/items/hideplayers/HidePlayers.java
@@ -1,8 +1,48 @@
 package me.kate.lobby.items.hideplayers;
 
-public class HidePlayers {
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+import me.kate.lobby.items.hideplayers.interfaces.Hideable;
+
+public class HidePlayers implements Hideable {
+	
+	private boolean hidden;
 
 	public HidePlayers() {
 		
+	}
+
+	@Override
+	public boolean isHidden(Player p) {
+		hidden = false; // get players setting
+		return hidden;
+	}
+
+	@Override
+	public void hide(Player p) {
+		for (Player online : Bukkit.getOnlinePlayers()) {
+			p.hidePlayer(online);
+		}
+		// set player settings to true
+	}
+
+	@Override
+	public void unhide(Player p) {
+		for (Player online : Bukkit.getOnlinePlayers()) {
+			p.showPlayer(online);
+		}
+		// set player settings to false
+	}
+
+	// might be useless lol
+	@Override
+	public void setHidden(boolean hide, Player p) {
+		if (hide) {
+			
+		}
+		if (!hide) {
+			
+		}
 	}
 }
