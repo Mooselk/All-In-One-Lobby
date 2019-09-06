2cfb1920878b2ef7716a08134f6dc8f5aa569a1e
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-06 12:26:42 -0700

    Reload selector.yml

--- src/me/kate/lobby/commands/ReloadConfigCommand.java
+++ src/me/kate/lobby/commands/ReloadConfigCommand.java
@@ -5,19 +5,25 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.kate.lobby.Main;
-import me.kate.lobby.utils.LocalServerPinger;
+import me.kate.lobby.data.files.SelectorFile;
+import me.kate.lobby.data.files.interfaces.ISelectorSettings;
 import net.md_5.bungee.api.ChatColor;
 
 public class ReloadConfigCommand implements CommandExecutor {
 
+	private ISelectorSettings sf = new SelectorFile();
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		final Player p = (Player) sender;
-		if (cmd.getName().equalsIgnoreCase("reloadconfig")) {
+		if (cmd.getName().equalsIgnoreCase("reloadselector")) {
 			if (p.isOp()) {
-				Main.getInstance().reloadConfig();
-				p.sendMessage(ChatColor.RED + "Reloading config! " + LocalServerPinger.getPing(p));
+				sf.reload();
+				if (!sf.reloadFailed()) {
+					p.sendMessage(ChatColor.RED + "Reloaded selector! ");
+				} else {
+					p.sendMessage(ChatColor.RED + "Reload failed, Check console for errors! ");
+				}
 			}
 		}
 		return true;
