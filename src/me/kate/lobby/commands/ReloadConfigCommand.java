df1aceff49c41e54908ef521a32a0ce2649f4a34
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-10 10:27:23 -0700

    Update ReloadConfigCommand.java

--- src/me/kate/lobby/commands/ReloadConfigCommand.java
+++ src/me/kate/lobby/commands/ReloadConfigCommand.java
@@ -1,19 +1,14 @@
 package me.kate.lobby.commands;
 
-import java.util.Arrays;
-
 import org.bukkit.Bukkit;
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import me.kate.lobby.Main;
 //import me.kate.lobby.data.files.SelectorFile;
 //import me.kate.lobby.data.files.interfaces.ISelectorSettings;
 import me.kate.lobby.npcs.api.NPC;
-import net.md_5.bungee.api.ChatColor;
 
 public class ReloadConfigCommand implements CommandExecutor {
 
@@ -25,9 +20,6 @@ public class ReloadConfigCommand implements CommandExecutor {
 		final Player p = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("reloadselector")) {
 			if (p.isOp()) {
-				
-				this.createFactions(p);
-				this.createSkyblock(p);
 //				sf.reload();
 //				if (!sf.reloadFailed()) {
 //					p.sendMessage(ChatColor.RED + "Reloaded selector! ");
@@ -37,26 +29,6 @@ public class ReloadConfigCommand implements CommandExecutor {
 			}
 		}
 		return true;
-	}
-	
-	public void createFactions(Player p) {
-		NPC npc = Main.getInstance().getNPCLib().createNPC(Arrays.asList(ChatColor.WHITE + "Hi there (Factions)", ChatColor.YELLOW + "Click on me!"));
-		npc.setLocation(new Location(Bukkit.getWorld("world"), -13.5 + 1, 61, 0.5 + 1));
-        Main.NPCS.put(npc.getId(), "Factions");
-        Bukkit.getLogger().info("IDS: " +  Main.IDS);
-        npc.create();
-        npc.show(p);
-        //showToAllPlayers(npc);
-	}
-	
-	public void createSkyblock(Player p) {
-		NPC npc = Main.getInstance().getNPCLib().createNPC(Arrays.asList(ChatColor.WHITE + "Hi there (SkyBlock)", ChatColor.YELLOW + "Click on me!"));
-		npc.setLocation(new Location(Bukkit.getWorld("world"), -13.5, 61, 0.5));
-        Main.NPCS.put(npc.getId(), "SkyBlock");
-        Bukkit.getLogger().info("IDS: " +  Main.NPCS);
-        npc.create();
-        npc.show(p);
-        //showToAllPlayers(npc);
 	}
 	
 	public void showToAllPlayers(NPC npc) {
