3df50189ef8660c243c5eab24e530d7a852570e3
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-16 07:37:48 -0700

    Update InteractNPCEvent.java

--- src/me/kate/lobby/events/InteractNPCEvent.java
+++ src/me/kate/lobby/events/InteractNPCEvent.java
@@ -11,19 +11,23 @@ import me.kate.lobby.data.files.NPCFile;
 import me.kate.lobby.npcs.api.events.NPCInteractEvent;
 
 public class InteractNPCEvent implements Listener {
-	
+
 	@EventHandler
-    public void onNPCInteract(NPCInteractEvent event) {
+	public void onNPCInteract(NPCInteractEvent event) {
 		Player p = event.getWhoClicked().getPlayer();
-        String id = Main.NPCINFO.get(event.getNPC().getId());
-        for (String npc : NPCFile.getNPCConfig().getConfigurationSection("npcs").getKeys(false)) {
-        	if (id.equalsIgnoreCase(npc)) {
-            	ConfigurationSection section = NPCFile.getNPCConfig().getConfigurationSection("npcs." + npc);
-            	String command = section.getString("command");
-            	String msg = ChatColor.translateAlternateColorCodes('&', section.getString("message"));
-            	p.performCommand(command);
-            	p.sendMessage(msg);
-            }
-        }
-    }
+		String id = Main.NPCINFO.get(event.getNPC().getId());
+		for (String npc : NPCFile.getNPCConfig().getConfigurationSection("npcs").getKeys(false)) {
+			if (id.equalsIgnoreCase(npc)) {
+				ConfigurationSection section = NPCFile.getNPCConfig().getConfigurationSection("npcs." + npc);
+				String command = section.getString("command");
+				String msg = ChatColor.translateAlternateColorCodes('&', section.getString("message"));
+				if (!command.equalsIgnoreCase("none")) {
+					p.performCommand(command);
+				}
+				if (!msg.equalsIgnoreCase("none")) {
+					p.sendMessage(msg);
+				}
+			}
+		}
+	}
 }
