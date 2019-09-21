ede71c0de34d0477af37d34e33fd3876f3f2bebc
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-20 20:22:26 -0700

    Delete NPCs

--- src/me/kate/lobby/npcs/NPCBuilder.java
+++ src/me/kate/lobby/npcs/NPCBuilder.java
@@ -1,7 +1,9 @@
 package me.kate.lobby.npcs;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -20,7 +22,28 @@ public class NPCBuilder {
 
 	private Hologram hologram;
 
+	private final List<String> defaultLore = Arrays.asList("Edit this text", "In the npc config!");
+
 	public NPCBuilder() {
+	}
+
+	public void create(int skinId, String name, Location loc, Player p) {
+		NPCFile.getNPCConfig().createSection("npcs." + name);
+		NPCFile.getNPCConfig().set("npcs." + name + ".id", name);
+		NPCFile.getNPCConfig().set("npcs." + name + ".skin", skinId);
+		NPCFile.getNPCConfig().set("npcs." + name + ".holotext", defaultLore);
+		NPCFile.getNPCConfig().set("npcs." + name + ".message", "Default message!");
+		NPCFile.getNPCConfig().set("npcs." + name + ".command", "none");
+		NPCFile.getNPCConfig().createSection("npcs." + name + ".location");
+		NPCFile.getNPCConfig().set("npcs." + name + ".location.x", loc.getBlockX());
+		NPCFile.getNPCConfig().set("npcs." + name + ".location.y", loc.getBlockY());
+		NPCFile.getNPCConfig().set("npcs." + name + ".location.z", loc.getBlockZ());
+		NPCFile.getNPCConfig().set("npcs." + name + ".location.yaw", loc.getYaw());
+		NPCFile.getNPCConfig().set("npcs." + name + ".location.ptich", loc.getPitch());
+		NPCFile.save();
+		NPCFile.reload();
+		this.destroyAll(p);
+		this.build(p);
 	}
 
 	public void build(Player p) {
@@ -53,20 +76,44 @@ public class NPCBuilder {
 
 	}
 
-	public void destroy(NPC npc) {
-
+	public void destroy(String name) {
+		if (Main.NPCINFO.containsValue(name)) {
+			NPC npcs = getNPCById(getValue(Main.NPCINFO, name));
+		}
 	}
 
-	public void reloadNPCs(Player p, NPCBuilder npcb) {
-		npcb.destroyAll(p);
+	public String getValue(Map<String, String> map, String value) {
+		for (Map.Entry<String, String> s : map.entrySet()) {
+			if (s.getValue().equalsIgnoreCase(value)) {
+				String key = s.getKey();
+				return key;
+			}
+		}
+		return null;
+	}
+
+	public NPC getNPCById(String id) {
+		for (int i = 0; i < Main.NPCS.size(); i++) {
+			NPC match = Main.NPCS.get(i);
+			Bukkit.getLogger().info("NPC2: " + Main.NPCS.get(i));
+			if (match.getId().equals(id)) {
+				return match;
+			}
+		}
+		return null;
+	}
+
+	public void reloadNPCs(Player p, NPCBuilder npcb, boolean msg) {
+		this.destroyAll(p);
 		NPCFile.reload();
-		npcb.build(p);
+		this.build(p);
 		new BukkitRunnable() {
 			@Override
 			public void run() {
 				npcb.showAll(true, null);
-				p.sendMessage("NPC-info:" + Main.NPCINFO);
-				p.sendMessage("Reload complete!");
+				if (msg) {
+					p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&f[&6NPC&f] Reload complete!"));
+				}
 			}
 
 		}.runTaskLater(Main.getInstance(), 20);
