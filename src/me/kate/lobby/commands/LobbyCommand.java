606ad07bd1c1858e305d52006280272b442d76ab
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-21 14:37:47 -0700

    Update LobbyCommand.java

--- src/me/kate/lobby/commands/LobbyCommand.java
+++ src/me/kate/lobby/commands/LobbyCommand.java
@@ -12,14 +12,20 @@ public class LobbyCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		final Player p = (Player) sender;
 		if (cmd.getName().equalsIgnoreCase("lobby")) {
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&m------------&r[ &l&6Lobby Help&r ]&m------------"));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/spawn &f- Sends you to lobby spawn point."));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/setspawn &f- Sets lobby spawn point."));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/npc help &f- Lists all NPC commands."));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/portal help &f - Lists all portal commands."));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/lobby reload <config> &f- Reload specifed config."));
-			p.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
+			if (p.hasPermission("lobby.help")) {
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&',
+						"&m------------&r[ &l&6Lobby Help&r ]&m------------"));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
+				p.sendMessage(
+						ChatColor.translateAlternateColorCodes('&', "&6/spawn &f- Sends you to lobby spawn point."));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/setspawn &f- Sets lobby spawn point."));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6/npc help &f- Lists all NPC commands."));
+				p.sendMessage(
+						ChatColor.translateAlternateColorCodes('&', "&6/portal help &f - Lists all portal commands."));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&',
+						"&6/lobby reload <config> &f- Reload specifed config."));
+				p.sendMessage(ChatColor.translateAlternateColorCodes('&', ""));
+			}
 		}
 		return false;
 	}
