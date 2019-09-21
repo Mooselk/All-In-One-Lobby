e2fd6d63ef790ddb05a499db62e46dd675b342b2
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-21 14:33:20 -0700

    NPC interact cooldown

--- src/me/kate/lobby/events/InteractNPCEvent.java
+++ src/me/kate/lobby/events/InteractNPCEvent.java
@@ -8,24 +8,32 @@ import org.bukkit.event.Listener;
 
 import me.kate.lobby.Main;
 import me.kate.lobby.data.files.NPCFile;
+import me.kate.lobby.items.portals.SendToServer;
+import me.kate.lobby.managers.CooldownManager;
 import me.kate.lobby.npcs.api.events.NPCInteractEvent;
 
 public class InteractNPCEvent implements Listener {
 
+	private final CooldownManager cooldownManager = new CooldownManager();
+
 	@EventHandler
 	public void onNPCInteract(NPCInteractEvent event) {
-		Player p = event.getWhoClicked().getPlayer();
+		final Player p = event.getWhoClicked().getPlayer();
 		String id = Main.NPCINFO.get(event.getNPC().getId());
-		for (String npc : NPCFile.getNPCConfig().getConfigurationSection("npcs").getKeys(false)) {
-			if (id.equalsIgnoreCase(npc)) {
-				ConfigurationSection section = NPCFile.getNPCConfig().getConfigurationSection("npcs." + npc);
-				String command = section.getString("command");
-				String msg = ChatColor.translateAlternateColorCodes('&', section.getString("message"));
-				if (!command.equalsIgnoreCase("none")) {
-					p.performCommand(command);
-				}
-				if (!msg.equalsIgnoreCase("none")) {
-					p.sendMessage(msg);
+		int timeLeft = cooldownManager.getCooldown(p.getUniqueId());
+		if (timeLeft == 0) {
+			cooldownManager.startCooldown(p, NPCFile.getNPCConfig().getInt("cooldown"));
+			for (String npc : NPCFile.getNPCConfig().getConfigurationSection("npcs").getKeys(false)) {
+				if (id.equalsIgnoreCase(npc)) {
+					ConfigurationSection section = NPCFile.getNPCConfig().getConfigurationSection("npcs." + npc);
+					String server = section.getString("server");
+					String msg = ChatColor.translateAlternateColorCodes('&', section.getString("message"));
+					if (!server.equalsIgnoreCase("none")) {
+						SendToServer.send(p, server);
+					}
+					if (!msg.equalsIgnoreCase("none")) {
+						p.sendMessage(msg);
+					}
 				}
 			}
 		}
