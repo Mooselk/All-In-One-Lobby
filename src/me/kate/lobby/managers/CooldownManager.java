410faa9beee4eac9810024d3b686ab938feb7151
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-09-21 14:33:51 -0700

    If cooldown is 0 revert to default

--- src/me/kate/lobby/managers/CooldownManager.java
+++ src/me/kate/lobby/managers/CooldownManager.java
@@ -2,15 +2,15 @@ package me.kate.lobby.managers;
 
 import java.util.UUID;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
 import me.kate.lobby.Main;
-import me.kate.lobby.data.files.HidePlayersFile;
-import me.kate.lobby.data.files.interfaces.IHidePlayerSettings;
 
 public class CooldownManager {
-
-	private static IHidePlayerSettings hf = new HidePlayersFile();
 	
-	public static final int DEFAULT_COOLDOWN = hf.getCooldownLength();
+	public static final int DEFAULT_COOLDOWN = 1;
 	
 	public void setCooldown(UUID player, int time) {
 		if (time < 1) {
@@ -22,5 +22,24 @@ public class CooldownManager {
 
 	public int getCooldown(UUID player) {
 		return Main.COOLDOWNS.getOrDefault(player, 0);
+	}
+	
+	public void startCooldown(Player p, int cooldown) {
+		if (cooldown == 0) {
+			setCooldown(p.getUniqueId(), CooldownManager.DEFAULT_COOLDOWN);
+			Bukkit.getLogger().warning("[Lobby] Cooldown was 0, using default (1 seconds).");
+		} else {
+			setCooldown(p.getUniqueId(), cooldown);
+		}
+		new BukkitRunnable() {
+			@Override
+			public void run() {
+				int timeLeft = getCooldown(p.getUniqueId());
+				setCooldown(p.getUniqueId(), --timeLeft);
+				if (timeLeft == 0) {
+					this.cancel();
+				}
+			}
+		}.runTaskTimer(Main.getInstance(), 20, 20);
 	}
 }
