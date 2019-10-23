2a4154ffc5d7d56cc57b0755950d0dc4ede58096
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-10-23 08:26:31 -0700

    Update Hideable.java

--- src/main/java/me/kate/lobby/items/hideplayers/interfaces/Hideable.java
+++ src/main/java/me/kate/lobby/items/hideplayers/interfaces/Hideable.java
@@ -1,14 +1,15 @@
 package me.kate.lobby.items.hideplayers.interfaces;
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 
 public interface Hideable {
 	
 	boolean isHidden(Player p);
 	
-	void hide(Player p);
+	void hide(Player p, ConfigurationSection section);
 	
-	void unhide(Player p);
+	void unhide(Player p, ConfigurationSection section);
 	
 	void setHidden(boolean hide, Player p);
 }
