71b4bbf670605584925c215b26e85caa5370d19f
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-11-01 12:43:05 -0700

    Logger util

---
+++ src/main/java/me/kate/lobby/utils/Logger.java
@@ -0,0 +1,28 @@
+package me.kate.lobby.utils;
+
+import org.bukkit.Server;
+
+import me.kate.lobby.Main;
+
+public class Logger {
+	
+	private static final Server SERVER = Main.getInstance().getServer();
+	
+	public static void debug(String msg) {
+		if (Main.DEBUG) { 
+			SERVER.getLogger().info(msg);
+		}
+	}
+	
+	public static void info(String msg) {
+		SERVER.getLogger().info(msg);
+	}
+	
+	public static void warn(String msg) {
+		SERVER.getLogger().warning(msg);
+	}
+	
+	public static void severe(String msg) {
+		SERVER.getLogger().severe(msg);
+	}
+}
