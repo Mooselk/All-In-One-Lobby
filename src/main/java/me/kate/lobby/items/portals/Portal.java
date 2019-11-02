b32a486648aeb7626542cb09bd02c7aef5805460
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-11-01 21:20:17 -0700

    Added null check

--- src/main/java/me/kate/lobby/items/portals/Portal.java
+++ src/main/java/me/kate/lobby/items/portals/Portal.java
@@ -5,7 +5,7 @@ import org.bukkit.Location;
 import org.bukkit.configuration.ConfigurationSection;
 
 import me.kate.lobby.Main;
-import me.kate.lobby.data.files.PortalsFile;
+import me.kate.lobby.data.files.PortalsConfig;
 import me.kate.lobby.items.portals.utils.Cuboid;
 
 public class Portal {
@@ -17,25 +17,27 @@ public class Portal {
 		if (!Main.PORTALS.isEmpty()) {
 			Main.PORTALS.clear();
 		}
-		for (String key : PortalsFile.getPortalConfig().getConfigurationSection("portals").getKeys(false)) {
-			final ConfigurationSection section = PortalsFile.getPortalConfig().getConfigurationSection("portals." + key);
-			final Location loc1 = new Location(Bukkit.getWorld(
-					section.getString("world")),
-					section.getInt("loc-1.x"),
-					section.getInt("loc-1.y"),
-					section.getInt("loc-1.z"));
-			final Location loc2 = new Location(Bukkit.getWorld(
-					section.getString("world")),
-					section.getInt("loc-2.x"),
-					section.getInt("loc-2.y"),
-					section.getInt("loc-2.z"));
-			Main.PORTALS.put(key, new Cuboid(loc1, loc2));
+		if (PortalsConfig.getPortalConfig().getConfigurationSection("portals") != null) {
+			for (String key : PortalsConfig.getPortalConfig().getConfigurationSection("portals").getKeys(false)) {
+				final ConfigurationSection section = PortalsConfig.getPortalConfig().getConfigurationSection("portals." + key);
+				final Location loc1 = new Location(Bukkit.getWorld(
+						section.getString("world")),
+						section.getInt("loc-1.x"),
+						section.getInt("loc-1.y"),
+						section.getInt("loc-1.z"));
+				final Location loc2 = new Location(Bukkit.getWorld(
+						section.getString("world")),
+						section.getInt("loc-2.x"),
+						section.getInt("loc-2.y"),
+						section.getInt("loc-2.z"));
+				Main.PORTALS.put(key, new Cuboid(loc1, loc2));
+			}
 		}
 	}
 	
 	public void reload() {
-		for (String key : PortalsFile.getPortalConfig().getConfigurationSection("portals").getKeys(false)) {
-			final ConfigurationSection section = PortalsFile.getPortalConfig().getConfigurationSection("portals." + key);
+		for (String key : PortalsConfig.getPortalConfig().getConfigurationSection("portals").getKeys(false)) {
+			final ConfigurationSection section = PortalsConfig.getPortalConfig().getConfigurationSection("portals." + key);
 			if (!Main.PORTALS.containsKey(key)) {
 				final Location loc1 = new Location(Bukkit.getWorld(
 						section.getString("world")),
@@ -56,8 +58,8 @@ public class Portal {
 		if (!Main.PORTALS.isEmpty()) {
 			Main.PORTALS.clear();
 		}
-		for (String key : PortalsFile.getPortalConfig().getConfigurationSection("portals").getKeys(false)) {
-			final ConfigurationSection section = PortalsFile.getPortalConfig().getConfigurationSection("portals." + key);
+		for (String key : PortalsConfig.getPortalConfig().getConfigurationSection("portals").getKeys(false)) {
+			final ConfigurationSection section = PortalsConfig.getPortalConfig().getConfigurationSection("portals." + key);
 				final Location loc1 = new Location(Bukkit.getWorld(
 						section.getString("world")),
 						section.getInt("loc-1.x"),
@@ -73,16 +75,16 @@ public class Portal {
 		}
 	
 	public void create(Location pos1, Location pos2, String name, String world, String server) {
-		PortalsFile.portalconf.set("portals." + name + ".world", world);
-		PortalsFile.portalconf.set("portals." + name + ".server", server);
-		PortalsFile.portalconf.set("portals." + name + ".loc-1.x", pos1.getBlockX());
-		PortalsFile.portalconf.set("portals." + name + ".loc-1.y", pos1.getBlockY());
-		PortalsFile.portalconf.set("portals." + name + ".loc-1.z", pos1.getBlockZ());
-		PortalsFile.portalconf.set("portals." + name + ".loc-2.x", pos2.getBlockX());
-		PortalsFile.portalconf.set("portals." + name + ".loc-2.y", pos2.getBlockY());
-		PortalsFile.portalconf.set("portals." + name + ".loc-2.z", pos2.getBlockZ());
-		PortalsFile.save();
-		PortalsFile.reload();
+		PortalsConfig.portalconf.set("portals." + name + ".world", world);
+		PortalsConfig.portalconf.set("portals." + name + ".server", server);
+		PortalsConfig.portalconf.set("portals." + name + ".loc-1.x", pos1.getBlockX());
+		PortalsConfig.portalconf.set("portals." + name + ".loc-1.y", pos1.getBlockY());
+		PortalsConfig.portalconf.set("portals." + name + ".loc-1.z", pos1.getBlockZ());
+		PortalsConfig.portalconf.set("portals." + name + ".loc-2.x", pos2.getBlockX());
+		PortalsConfig.portalconf.set("portals." + name + ".loc-2.y", pos2.getBlockY());
+		PortalsConfig.portalconf.set("portals." + name + ".loc-2.z", pos2.getBlockZ());
+		PortalsConfig.save();
+		PortalsConfig.reload();
 		this.reload();
 		if (!Main.SELECTIONS.isEmpty()) {
 			Main.SELECTIONS.clear();
@@ -90,9 +92,9 @@ public class Portal {
 	}
 	
 	public void delete(String name) {
-		PortalsFile.portalconf.set("portals." + name, null);
-		PortalsFile.save();
-		PortalsFile.reload();
+		PortalsConfig.portalconf.set("portals." + name, null);
+		PortalsConfig.save();
+		PortalsConfig.reload();
 		this.reloadAll();
 	}
 }
