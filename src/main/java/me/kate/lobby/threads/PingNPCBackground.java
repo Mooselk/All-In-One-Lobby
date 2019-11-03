1879b9d63722a3187dadf0bbd0f3e6257c1e86f1
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-11-02 20:14:49 -0700

    Ping servers for live playercount above NPC

---
+++ src/main/java/me/kate/lobby/threads/PingNPCBackground.java
@@ -0,0 +1,63 @@
+package me.kate.lobby.threads;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+
+import me.kate.lobby.Main;
+import me.kate.lobby.data.files.NPCConfig;
+import me.kate.lobby.npcs.tasks.HoloTextTask;
+import me.kate.lobby.utils.MineStat;
+
+public class PingNPCBackground extends Thread {
+
+	private HoloTextTask holo = new HoloTextTask();
+
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				if (!Bukkit.getOnlinePlayers().isEmpty()) {
+					if (NPCConfig.getNPCConfig().getConfigurationSection("npcs") != null) {
+						for (final String key : NPCConfig.getNPCConfig().getConfigurationSection("npcs").getKeys(false)) {
+							final ConfigurationSection section = NPCConfig.getNPCConfig().getConfigurationSection("npcs." + key + ".server");
+							try {
+								Thread.sleep(4000); // default: 2000
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							if (!section.getBoolean("live-player-count", false)) {
+								continue;
+							}
+
+							String ip = section.getString("ip");
+							int port = section.getInt("port");
+
+							MineStat ms = new MineStat(ip, port, 1);
+							Map<String, Object> serverInfo = new HashMap<>();
+
+							String serverName = section.getString("server-name");
+							serverInfo.put("isOnline", ms.isServerUp());
+							if (ms.isServerUp()) {
+								serverInfo.put("online", ms.getCurrentPlayers());
+							}
+							Main.NPC_PLACEHOLDERS.put(serverName, serverInfo);
+							holo.updateText(section.getString("server-name"), key);
+						}
+					}
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+				// So the loop doesn't break if an error occurs
+				// Print the error, sleep, try again.
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e2) {
+					e2.printStackTrace();
+				}
+			}
+		}
+	}
+}
