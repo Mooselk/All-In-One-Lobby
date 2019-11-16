4dd7e81550144e3b2a665b0c32104d8fed6a1782
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2019-11-15 21:37:08 -0800

    TabList NMS classes

---
+++ src/main/java/me/kate/lobby/npcs/nms/v1_11_R1/TabList_v1_11_R1.java
@@ -0,0 +1,40 @@
+package me.kate.lobby.npcs.nms.v1_11_R1;
+
+import java.lang.reflect.Field;
+
+import org.bukkit.craftbukkit.v1_11_R1.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+import me.kate.lobby.modules.tablist.TabList;
+import net.minecraft.server.v1_11_R1.IChatBaseComponent;
+import net.minecraft.server.v1_11_R1.IChatBaseComponent.ChatSerializer;
+import net.minecraft.server.v1_11_R1.PacketPlayOutPlayerListHeaderFooter;
+
+public class TabList_v1_11_R1 implements TabList {
+	
+	@Override
+	public void sendHeaderFooter(Player player, String header, String footer) {    
+		IChatBaseComponent tabHeader = ChatSerializer.a("{\"text\": \"" + header + "\"}");
+        IChatBaseComponent tabFooter = ChatSerializer.a("{\"text\": \"" + footer + "\"}");
+        
+        PacketPlayOutPlayerListHeaderFooter packet = new PacketPlayOutPlayerListHeaderFooter();
+        
+        try {
+        	
+            Field headerField = packet.getClass().getDeclaredField("a");
+            headerField.setAccessible(true);
+            headerField.set(packet, tabHeader);
+            headerField.setAccessible(false);
+            
+            Field footerField = packet.getClass().getDeclaredField("b");
+            footerField.setAccessible(true);
+            footerField.set(packet, tabFooter);
+            footerField.setAccessible(false);
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        ((CraftPlayer) player).getHandle().playerConnection.sendPacket(packet);
+    }
+}
