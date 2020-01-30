a6db43a07fa40ae1cebf93505f0109daac850fb6
Kaitlyn <44407652+Mooselk@users.noreply.github.com>
2020-01-30 10:28:59 -0800

    Get NPCSlot enum from string

--- src/main/java/me/kate/lobby/npcs/api/state/NPCSlot.java
+++ src/main/java/me/kate/lobby/npcs/api/state/NPCSlot.java
@@ -1,16 +1,32 @@
 package me.kate.lobby.npcs.api.state;
 
+import java.util.Map;
+
+import com.google.common.collect.Maps;
+
 public enum NPCSlot {
 
     HELMET(4),
     CHESTPLATE(3),
     LEGGINGS(2),
     BOOTS(1),
-    MAINHAND(0),
+    HAND(0),
     OFFHAND(5);
 
     int slot;
 
+    private final static Map<String, NPCSlot> BY_NAME = Maps.newHashMap();
+    
+    static {
+        for (NPCSlot slot : values()) {
+            BY_NAME.put(slot.name(), slot);
+        }
+    }
+    
+    public static NPCSlot getSlot(final String name) {
+    	return BY_NAME.get(name);
+    }
+    
     NPCSlot(int slot) {
         this.slot = slot;
     }
