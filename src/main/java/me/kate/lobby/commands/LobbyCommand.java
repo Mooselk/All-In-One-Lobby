81248f9ca8a9c38d24f3940837297e0a121d113e
Mooselk <44407652+Mooselk@users.noreply.github.com>
2020-06-19 21:25:29 -0700

    Rewrote commands 2/3

--- src/main/java/me/kate/lobby/commands/LobbyCommand.java
+++ src/main/java/me/kate/lobby/commands/LobbyCommand.java
@@ -1,6 +1,5 @@
 package me.kate.lobby.commands;
 
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,89 +7,130 @@ import org.bukkit.entity.Player;
 
 import me.kate.lobby.Main;
 import me.kate.lobby.Messages;
+import me.kate.lobby.Permissions;
 import me.kate.lobby.data.Config;
 import me.kate.lobby.data.files.PluginConfig;
 import me.kate.lobby.data.files.SelectorConfig;
 import me.kate.lobby.data.files.ToggleConfig;
 import me.kate.lobby.modules.Spawn;
 import me.kate.lobby.modules.selector.Selector;
-import me.kate.lobby.tasks.bungee.HeartBeat;
-import me.kate.lobby.utils.Utils;
 
 public class LobbyCommand implements CommandExecutor {
 
-	private Config selectorConfig = new SelectorConfig();
-	private Config mainConfig = new PluginConfig();
-	private ToggleConfig togglePlayersConfig = new ToggleConfig();
-	
-	private Selector selector = Main.getInstance().getSelector();
-	
+	private Main plugin;
+	private Config selectorConfig;
+	private Config mainConfig;
+	private Config togglePlayersConfig;
+	private Selector selector;
+	private Messages messages;
+
+	public LobbyCommand(Main plugin) {
+		this.plugin = plugin;
+		this.selectorConfig = new SelectorConfig();
+		this.mainConfig = new PluginConfig();
+		this.togglePlayersConfig = new ToggleConfig();
+		this.selector = plugin.getSelector();
+		this.messages = new Messages();
+	}
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		final Player player = (Player) sender;
-		if (cmd.getName().equalsIgnoreCase("lobby")) {
-			if (args.length == 0) {
-				if (player.hasPermission("lobby.help")) {
-					Messages.lobbyHelp(player);
-				}
-			} else {
-				if (args[0].equalsIgnoreCase("test")) {
-					HeartBeat.fetchServerStatusFromAsync("localhost", 25566, status -> {
-					System.out.println("ServerStatus: " + status.toString());
-				});
-				}
-				if (args[0].equalsIgnoreCase("help")) {
-					if (player.hasPermission("lobby.npc.help")) {
-						Messages.lobbyHelp(player);
-					} else {
-						Messages.noPermission(player);
-					}
-				}
-				if (args[0].equalsIgnoreCase("setspawn")) {
-					if (player.hasPermission("lobby.setspawn")) {
-						Location loc = player.getLocation();
-						Spawn.setSpawn(player, loc);
-					} else {
-						Messages.noPermission(player);
-					}
-				}
-				if (args[0].equalsIgnoreCase("spawn")) {
-					if (player.hasPermission("lobby.spawn")) {
-						player.teleport(Spawn.getSpawn());
-						Messages.send("&f[&6Lobby&f] Teleporting to spawn...", player);
-					} else {
-						Messages.noPermission(player);
 
-					}
-				}
-				if (args[0].equalsIgnoreCase("reload")) {
-					if (player.hasPermission("lobby.reload")) {
-						if (args.length > 1) {
-							switch (args[1]) {
-							case "playerhider":
-								Utils.reloadConfig(togglePlayersConfig, player);
-								break;
-							case "selector":
-								Utils.reloadConfig(selectorConfig, player);
-								selector.delete();
-								selector.reload();
-								break;
-							case "config":
-								Utils.reloadConfig(mainConfig, player);
-								if (mainConfig.getConfig().getBoolean("tablist.enabled")) {
-									Main.getInstance().getTabList().update();
-								}
-								break;
-							default:
-								Messages.send("Unknown config: " + args[1].toLowerCase(), player);
-							}
-						} else {
-							Messages.lobbyHelpReload(player);
-						}
-					}
-				}
-			}
+		if (!cmd.getName().equalsIgnoreCase("lobby")) {
+			return false;
 		}
+
+		if (args.length == 0) {
+			messages.lobbyHelp(sender);
+			return false;
+		}
+
+		switch (args[0]) {
+
+		case "help": {
+			if (!Permissions.LOBBY_HELP.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			messages.lobbyHelp(sender);
+			break;
+		}
+
+		case "setspawn": {
+			if (!Permissions.LOBBY_SETSPAWN.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+
+			if (!(sender instanceof Player)) {
+				messages.consoleSender(sender);
+				break;
+			}
+
+			Spawn.setSpawn((Player) sender);
+			break;
+		}
+
+		case "spawn": {
+			if (!Permissions.LOBBY_SPAWN.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+
+			if (!(sender instanceof Player)) {
+				messages.consoleSender(sender);
+				break;
+			}
+
+			Spawn.toSpawn((Player) sender);
+			break;
+		}
+
+		case "reload": {
+
+			if (!Permissions.LOBBY_HELP.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+
+			if (args.length > 1) {
+				switch (args[1]) {
+
+				case "playerhider": {
+					togglePlayersConfig.reloadConfig(togglePlayersConfig, (Player) sender);
+					break;
+				}
+
+				case "selector": {
+					selectorConfig.reloadConfig(selectorConfig, (Player) sender);
+					selector.reload();
+					break;
+				}
+
+				case "config": {
+					mainConfig.reloadConfig(mainConfig, (Player) sender);
+					if (mainConfig.getConfig().getBoolean("tablist.enabled"))
+						plugin.getTabList().update();
+					break;
+				}
+
+				default:
+					messages.send("Unknown config: " + args[1].toLowerCase(), sender);
+				}
+
+			} else {
+				messages.lobbyHelpReload(sender);
+			}
+
+			break;
+		}
+
+		default: {
+			messages.lobbyHelp(sender);
+		}
+
+		}
+
 		return true;
 	}
 }
--- src/main/java/me/kate/lobby/commands/PortalCommand.java
+++ src/main/java/me/kate/lobby/commands/PortalCommand.java
@@ -1,6 +1,5 @@
 package me.kate.lobby.commands;
 
-import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -8,110 +7,228 @@ import org.bukkit.entity.Player;
 
 import me.kate.lobby.Main;
 import me.kate.lobby.Messages;
-import me.kate.lobby.data.Config;
+import me.kate.lobby.Permissions;
 import me.kate.lobby.data.files.PortalsConfig;
 import me.kate.lobby.modules.portals.Portal;
-import me.kate.lobby.modules.portals.PortalLocation;
+import me.kate.lobby.modules.portals.select.Selection;
+import me.kate.lobby.modules.portals.select.SelectionVisualizer;
 import me.kate.lobby.modules.portals.utils.PortalWand;
 import me.kate.lobby.utils.Logger;
 
-public class PortalCommand extends PortalLocation implements CommandExecutor {
+public class PortalCommand implements CommandExecutor {
 
-	private Config portalConfig = new PortalsConfig();
-	private final Portal portal = new Portal();
-	
-	public PortalCommand() {
-		super(null, null);
+	private Main plugin;
+	private PortalsConfig portalConfig;
+	private Portal portal;
+	private Messages messages;
+	private Selection selections;
+
+	public PortalCommand(Main plugin) {
+		this.plugin = plugin;
+		this.portalConfig = new PortalsConfig();
+		this.portal = new Portal();
+		this.messages = new Messages();
+		this.selections = new Selection();
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		final Player player = (Player) sender;
-		if (cmd.getName().equalsIgnoreCase("portal")) {
-			if (args.length == 0) {
-				Messages.portalHelp(player);
-			} else {
-				if (args[0].equalsIgnoreCase("help")) {
-					if (player.hasPermission("lobby.portal.help"))
-						Messages.portalHelp(player);
-					else 
-						Messages.noPermission(player);
-				}
+		Player player = null;
+		
+		if (sender instanceof Player) {
+			player = (Player) sender;
+		}
+		
+		Selection selection = null;
+		SelectionVisualizer visualizer = null;
+		
+		if (!cmd.getName().equalsIgnoreCase("portal")) {
+			return true;
+		}
+		
+		if (args.length == 0) {
+			messages.portalHelp(sender);
+			return true;
+		}
+		
+		if (player != null) {
+			selection = selections.getSelection(player.getUniqueId());
+			visualizer = new SelectionVisualizer(plugin, player);
+		}
+		
+		switch(args[0]) {
+		
+		case "help" : {
+			
+			if (!Permissions.PORTAL_HELP.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			messages.portalHelp(sender);
+			return true;
+		}
+		
+		case "wand" : {
+			
+			if (!(sender instanceof Player)) {
+				Logger.info("This command can only be used by a player");
+				break;
+			}
+			
+			if (!Permissions.PORTAL_HELP.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			messages.send("&f[&6Portal&f] &5Left click: &dSelect pos #1; &5Right click: &dSelect pos #2", sender);
+			player.getInventory().addItem(PortalWand.WAND);
+			
+			return true;
+		}
+		
+		case "create" : {
+			
+			if (!(sender instanceof Player)) {
+				Logger.info("This command can only be used by a player");
+				break;
+			}
+			
+			if (!Permissions.PORTAL_CREATE.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			if (args.length < 3) {
+				messages.send("&6Usage: &f/portal create <portal_name> [server]", player);
+				break;
+			}
+			
+			if (args.length == 3) {
+				
+				String name = args[1];
+				String server = args[2];
+				String world = player.getWorld().getName();
 
-				if (args[0].equalsIgnoreCase("wand")) {
-					if (player.hasPermission("lobby.portal.wand")) {
-						Messages.send("&f[&6Portal&f] &5Left click: &dSelect pos #1; &5Right click: &dSelect pos #2", player);
-						player.getInventory().addItem(PortalWand.WAND);
-					} else Messages.noPermission(player);
-				}
+				if (selection != null 
+						&& selection.getPos1() != null 
+						&& selection.getPos2() != null) {
 
-				if (args[0].equalsIgnoreCase("create")) {
-					if (player.hasPermission("lobby.portal.create")) {
-						if (args.length < 3) {
-							Messages.send("&6Usage: &f/portal create <portal_name> <server>", player);
-						} else {
-							if (args.length == 3) {
-								String name = args[1];
-								String server = args[2];
-								String world = player.getWorld().getName();
-								
-								Location pos1 = fromPortal(getPlayerSelection("POS1", player), player, "POS1");
-								Location pos2 = fromPortal(getPlayerSelection("POS2", player), player, "POS2");
-								
-								if (pos1 != null && pos2 != null) {
-									
-									Logger.debug("Selection 1: " + getPlayerSelection("POS1", player) + " Selection 2: " + getPlayerSelection("POS2", player));				
-									Logger.debug("Location 1: " + pos1 + " Location 2: " + pos2);
-									
-									portal.create(pos1, pos2, name, world, server, player);
-									Messages.send("&f[&6Portal&f] Created portal '" + name + "'.", player);
-									
-								} else {
-									Messages.send("&f[&6Portal&f] Create a selection before creating a portal.", player);
-								}
-							}
-						}
-					} else Messages.noPermission(player);
-				}
+					Logger.debug("Selection 1: "
+					+ selection.getPos1()
+					+ " Selection 2: "
+					+ selection.getPos2());
+					
+					Logger.debug("Location 1: " 
+					+ selection.getPos1() 
+					+ " Location 2: " 
+					+ selection.getPos2());
 
-				if (args[0].equalsIgnoreCase("delete")) {
-					if (player.hasPermission("lobby.portal.delete")) {
-						if (args.length < 2) {
-							Messages.send("&6Usage: &f/portal delete <portal_name>", player);
-						} else {
-							if (args.length == 2) {
-								String name = args[1];
-								if (portalConfig.getConfig().getString("portals." + name) != null) {
-									portal.delete(name);
-									Messages.send("&f[&6Portal&f] Deleted portal '" + name + "'.", player);
-								} else {
-									Messages.send("&f[&6Portal&f] Error deleting portal '" + name + "'. Portal does not exist!", player);
-								}
-							}
-						}
-					} else Messages.noPermission(player);
+					portal.create(selection.getPos1(), selection.getPos2(), name, world, server, player);
+					messages.send("&f[&6Portal&f] Created portal '" + name + "'.", player);
+					break;
 				}
 				
-				if (args[0].equalsIgnoreCase("clear")) {
-					if (player.hasPermission("lobby.portal.clear")) {
-						if (!Main.getInstance().getPortals().isEmpty()) {
-							// Main.getInstance().getPortals().clear();
-							Messages.send("unfinished", player);
-							Messages.send("&f[&6Portal&f] Cleared last selection.", player);
-						} else {
-							Messages.send("&f[&6Portal&f] No selections to clear.", player);
-						}
-					} else Messages.noPermission(player);
-				}
-
-				if (args[0].equalsIgnoreCase("reload")) {
-					if (player.hasPermission("lobby.portal.reload")) {
-						portal.reloadAll();
-						Messages.send("&f[&6Portals&f] Reloading all portals.", player);
-					} else Messages.noPermission(player);
-				}
+				messages.send("&f[&6Portal&f] Create a selection before creating a portal.", player);
 			}
+			return true;
 		}
+		
+		case "delete" : {
+			
+			if (!Permissions.PORTAL_DELETE.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			if (args.length < 2) {
+				messages.send("&6Usage: &f/portal delete <portal_name>", sender);
+				break;
+			}
+			
+			if (args.length == 2) {
+				String name = args[1];
+				
+				if (portalConfig.getPortal(name) == null) {
+					messages.send("&f[&6Portal&f] Error deleting portal '" + name + "'. Portal does not exist!", sender);
+					break;
+				}
+				
+				portal.delete(name);
+				messages.send("&f[&6Portal&f] Deleted portal '" + name + "'.", sender);
+			}
+			return true;
+		}
+		
+		case "show" : {
+			
+			if (!(sender instanceof Player)) {
+				Logger.info("This command can only be used by a player");
+				break;
+			}
+			
+			if (!Permissions.PORTAL_SHOW.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			if (selection.getPos1() == null || selection.getPos2() == null) {
+				messages.send("&f[&6Portal&f] Select two points with the portalwand first.", player);
+			}
+			
+			visualizer.setVis();
+			
+			if (visualizer.isVisualizing()) {
+				
+				if (args.length == 2) {
+					
+				} else {
+					visualizer.setCornerA(selection.getPos1());
+					visualizer.setCornerB(selection.getPos2());
+				}
+				
+				visualizer.update();
+				
+			} else visualizer.stop();
+			return true;
+		}
+		
+		case "clear" : {
+			
+			if (!(sender instanceof Player)) {
+				Logger.info("This command can only be used by a player");
+				break;
+			}
+			
+			if (!Permissions.PORTAL_CLEAR.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			if (selection == null) {
+				messages.send("&f[&6Portal&f] No selections to clear.", sender);
+				break;
+			}
+			
+			selection.clear(player.getUniqueId());
+			messages.send("&f[&6Portal&f] Cleared last selection.", sender);
+			return true;
+		}
+		
+		case "reload" : {
+			
+			if (!Permissions.PORTAL_RELOAD.has(sender)) {
+				messages.noPermission(sender);
+				break;
+			}
+			
+			portal.reloadAll();
+			messages.send("&f[&6Portals&f] Reloading all portals.", sender);
+			return true;
+		}
+		
+		}
+		
 		return true;
 	}
 }
