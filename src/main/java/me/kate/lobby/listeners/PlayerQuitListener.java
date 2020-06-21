b668b9101e922e6a2f6e518cb656a474a9eda829
Mooselk <44407652+Mooselk@users.noreply.github.com>
2020-06-20 21:43:30 -0700

    Clear selections to prevent memory leak

--- src/main/java/me/kate/lobby/listeners/PlayerQuitListener.java
+++ src/main/java/me/kate/lobby/listeners/PlayerQuitListener.java
@@ -6,19 +6,29 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.kate.lobby.modules.portals.select.Selection;
+import me.kate.lobby.modules.portals.select.SelectionVisualizer;
 import me.kate.lobby.utils.Utils;
 
-public class PlayerLeaveListener implements Listener {
+public class PlayerQuitListener implements Listener {
 	
 	private JavaPlugin plugin;
+	private Selection selection;
+	private SelectionVisualizer visualizer;
 	
-	public PlayerLeaveListener(JavaPlugin plugin) {
+	public PlayerQuitListener(JavaPlugin plugin) {
 		this.plugin = plugin;
+		this.selection = new Selection();
+		this.visualizer = new SelectionVisualizer();
 	}
 	
 	@EventHandler
 	public void onLeave(final PlayerQuitEvent event) {
 		final Player player = event.getPlayer();
+		
+		selection.getSelections().remove(player.getUniqueId());
+		visualizer.getIntances().remove(player.getUniqueId());
+		
 		if (!plugin.getConfig().getString("options.custom-leavemsg").equals("none")) {
 			event.setQuitMessage(Utils.replacePlayer(plugin.getConfig().getString("options.custom-leavemsg"), player));
 		} else {
